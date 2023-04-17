e216dfe93583a32925a62d29559e11732bcfe49b
Chisom Iheme <chisompro@gmail.com>
2023-04-17 15:31:17 +0100

    Initials

---
+++ Data/Models/ToDoTask.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Data.Models
+{
+    public class ToDoTask
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
